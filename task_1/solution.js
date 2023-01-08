ea966ee2317f8fd97cf7f9746a00dbcdb77cde82
ChirkinKirill <chirkin.kirill@gmail.com>
2023-01-08 16:05:04 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,8 +1,11 @@
-function addInCartMessage(title, price) {
+function addInCartMessage(title, price) 
+{
     let productName = title;
     let productPrice = price;
-
+    
     // Задание №1.1. Оповещение о добавлении в корзину
+
+    let message = productName + ' ' + "за" + ' ' + productPrice + ' ' + "теперь в корзине!"
 
     // создайте переменную message
     // присвойте переменной message значение переменной productName
@@ -24,6 +27,8 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
+    let newValue = oldValue + 1
+
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -38,6 +43,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+
+    let newSum = oldSum + difference
+    let newSumText = `${newSum} ₽`
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
