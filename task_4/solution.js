7628f29a3fcbf48b8238c7cd6c85fa2fbd394479
ChirkinKirill <chirkin.kirill@gmail.com>
2023-01-30 22:48:44 +0300

    commit

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,7 +3,7 @@
 function parseProducts(json) {
 
     let data = JSON.parse(json)
-    let products = {data}
+    let products = products.data
     return products
 
     // Аргументом функции является JSON
@@ -16,6 +16,13 @@ function parseProducts(json) {
 function renderProductsCards(json){
     // дальнейшие действия требуется выполнять тут
     clearProducts()
+
+    let products = parseProducts(json)
+
+    let length = products.length
+        for (let i = 0; i < length; i += 1) {
+            addProduct(products[i]);
+     }
 
  }
 
